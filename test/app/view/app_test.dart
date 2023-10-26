d73754749628234b853de030f4c1a980fe17cd7a
Juriba <Juribaalsiari@gmail.com>
2023-10-26 23:55:01 +0300

    clean up

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ny_articles_clean_architecture/app/app.dart';
-import 'package:ny_articles_clean_architecture/counter/counter.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(Scaffold), findsOneWidget);
     });
   });
 }
