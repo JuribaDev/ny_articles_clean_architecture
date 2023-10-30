fb1b8fc006ef55d65a6b316038e6e41cb2b84c71
Juriba <Juribaalsiari@gmail.com>
2023-10-30 22:31:50 +0300

    hot fix

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ny_articles_clean_architecture/config/theme/color_scheme.dart';
-import 'package:ny_articles_clean_architecture/features/screens/most_viewed_articles_screen.dart';
+import 'package:ny_articles_clean_architecture/features/most_viewed_articles/presentation/screens/most_viewed_articles_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
--- lib/features/most_viewed_articles/presentation/screens/most_viewed_articles_screen.dart
+++ lib/features/most_viewed_articles/presentation/screens/most_viewed_articles_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ny_articles_clean_architecture/core/widgets/responsive_layout_wrapper.dart';
-import 'package:ny_articles_clean_architecture/features/presentation/bloc/most_viewed_article_bloc.dart';
-import 'package:ny_articles_clean_architecture/features/screens/mobile_portrait/most_viewed_articles_mobile_portrait_view.dart';
+import 'package:ny_articles_clean_architecture/features/most_viewed_articles/presentation/bloc/most_viewed_article_bloc.dart';
+import 'package:ny_articles_clean_architecture/features/most_viewed_articles/presentation/screens/mobile_portrait/most_viewed_articles_mobile_portrait_view.dart';
 import 'package:ny_articles_clean_architecture/injection_container.dart';
 
 class MostViewedArticlesScreen extends StatelessWidget {
